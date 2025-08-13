0b4639820236276f8a9b83b7bb7dd8ae6c6dab6a
닮 <dhdpekfa1@daum.net>
2025-08-13 19:49:59 +0900

    feat: 로그인 화면 퍼블리싱

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,19 +1,54 @@
-import { Dropdown } from '@/shared/ui';
-import { useState } from 'react';
+import { Button, LabelInput } from '@/shared/ui';
+import { useNavigate } from 'react-router';
 
 export const LoginPage = () => {
-  const [value, setValue] = useState('');
+  const navigate = useNavigate();
+
+  const onPressButton = () => {
+    console.log('TODO: 이벤트 구현');
+    navigate('/verification');
+  };
+
   return (
-    <div>
-      <div className='mr-200 mt-10 ml-10'>
-        <Dropdown
-          items={['A', 'B', 'C']}
-          selectedItem={value}
-          placeholder='선택'
-          onSelect={v => setValue(v)}
-          hasError={false}
-          errorMessage='필수 선택입니다.'
-          description='선택해주세요.'
+    <div className='min-h-dvh w-full flex flex-col justify-between lg:justify-center'>
+      <div className='w-full sm:max-w-xl lg:max-w-lg lg:mx-auto p-4'>
+        <div className='flex flex-col gap-4'>
+          <div className='flex gap-2 items-end'>
+            <h1 className='italic text-green-500 text-4xl font-black'>
+              ONE-pass
+            </h1>
+            <span className='mid-5 text-grey-600 lg:hidden'>
+              출석 사전 확인
+            </span>
+          </div>
+          <p className='title-6 text-grey-800'>
+            [학원명] 수업 출석 사전 확인 페이지입니다.
+          </p>
+          <p className='mid-5 text-grey-600'>
+            회원 이름과 학부모 전화번호를 입력해 학원 회원 인증을 진행해주세요.
+          </p>
+        </div>
+
+        <div className='mt-4 flex flex-col gap-4'>
+          <LabelInput
+            label='회원 이름'
+            placeholder='회원 이름을 입력해주세요.'
+            size='md'
+          />
+          <LabelInput
+            label='학부모 전화 번호'
+            placeholder='학부모 전화 번호를 입력해주세요.'
+            size='md'
+          />
+        </div>
+      </div>
+
+      <div className='button-shadow-container w-full lg:max-w-lg lg:mx-auto '>
+        <Button
+          title='확인'
+          variant='primary'
+          onPress={onPressButton}
+          size='lg'
         />
       </div>
     </div>
